4139153a5acbe7a9ef25ae1030b90e08fe99ab06
Bryce Fisher-Fleig <hckr1292@gmail.com>
2015-11-25 13:20:21 -0800

    Implements Shape parsing
    Includes tests for primitive shapes, but not more complex ones
    like structures, exceptions, string, and integers.

--- src/parser/shape.rs
+++ src/parser/shape.rs
@@ -12,8 +12,17 @@ pub struct Shape {
 }
 
 impl Shape {
-    fn parse(_name: String, json: Value) -> Result<Shape, ParseError> {
-        Err(ParseError::NotImplemented)
+    // TODO learn to use lifetimes here to avoid allocations
+    fn parse(name: &str, json: Value) -> Result<Shape, ParseError> {
+        let obj = match json.as_object() {
+            Some(obj) => obj,
+            None => return Err(ParseError::ExpectedObject)
+        };
+        let shape_type = try!(ShapeType::parse(obj));
+        Ok(Shape {
+            name: name.to_string(),
+            shape_type: shape_type
+        })
     }
 }
 
@@ -23,5 +32,55 @@ mod test {
     extern crate serde_json;
 
     use super::*;
+    use super::super::shape_type::*;
     use ::testhelpers::fixture_btreemap;
+
+    fn primitive_shape(name: &str) -> serde_json::Value {
+        fixture_btreemap("shapes/primitive-shapes").get(name).unwrap().clone()
+    }
+
+    #[test]
+    fn boolean() {
+        let output = Shape::parse("Boolean", primitive_shape("Boolean"));
+        assert_eq!(output, Ok(Shape {
+            name:"Boolean".to_string(),
+            shape_type: ShapeType::Boolean,
+        }));
+    }
+
+    #[test]
+    fn double() {
+        let output = Shape::parse("Double", primitive_shape("Double"));
+        assert_eq!(output, Ok(Shape {
+            name:"Double".to_string(),
+            shape_type: ShapeType::Double,
+        }));
+    }
+
+    #[test]
+    fn timestamp() {
+        let output = Shape::parse("Date", primitive_shape("Date"));
+        assert_eq!(output, Ok(Shape {
+            name:"Date".to_string(),
+            shape_type: ShapeType::Timestamp,
+        }));
+    }
+
+    #[test]
+    fn long() {
+        let output = Shape::parse("Long", primitive_shape("Long"));
+        assert_eq!(output, Ok(Shape {
+            name:"Long".to_string(),
+            shape_type: ShapeType::Long,
+        }));
+    }
+
+    #[test]
+    fn float() {
+        let output = Shape::parse("Float", primitive_shape("Float"));
+        assert_eq!(output, Ok(Shape {
+            name:"Float".to_string(),
+            shape_type: ShapeType::Float,
+        }));
+    }
 }
